8b09220bd0029d23b459ddd9e222e59b3fe6c02b
Kasum5 <70338595+Kasum5@users.noreply.github.com>
2021-05-31 20:51:36 +0300

    Update task.js

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,26 +1,50 @@
 // Задание 1
 function getArrayParams(arr) {
-  let min,max,sum,avg;
+  let min = 2;
+  let max = 3;
+  let sum = 0;
+  let avg = 0;
 
-  // Ваш код
-
+  min = Math.min.apply(null, arr);
+  max = Math.max.apply(null, arr);
+  
+  for(let i = 0; i < arr.length; i++) {
+    sum += arr[i];
+    avg = sum / arr.length;
+    avg.toFixed(2);
+  };
+  
   return { min:min, max:max, avg:avg };
+  // Ваш код
 }
+getArrayParams([99,-99,10])
+
 
 // Задание 2
 function worker(arr) {
   let sum;
 
   // Ваш код
-
+  let sum = 0;
+    for(let i = 0; i < arr.length; i++) {
+      sum += arr[i];
+    }
   return sum;
 }
 
 function makeWork(arrOfArr, func) {
-  let max
-
   // Ваш кода
   // for ...
+  let max = 0;
+  let b = [];
+  // Ваш кода
+  // for ...
+  for(let i = 0; i < arrOfArr.length; i++) {
+    b.push(worker(arrOfArr[i]));
+  };
   
+  max = Math.max.apply(null,b);
   return max
 }
+
+makeWork([[10,10,11],[20,10]], worker)
