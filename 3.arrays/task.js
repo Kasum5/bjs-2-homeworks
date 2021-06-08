851488676bf48f1e44f44320c9e86ec4989965f3
Kasum5 <70338595+Kasum5@users.noreply.github.com>
2021-06-08 16:57:52 +0300

    Update task.js

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,13 +1,19 @@
 function compareArrays(arr1, arr2) {
-  let result;
-
+  let result = (arr1.length == arr2.length) && arr1.every(function(element,index){
+    return element === arr2[index];
+  })
+  
   // Ваш код
 
   return result; // boolean
 }
 
 function advancedFilter(arr) {
-  let resultArr;
+  let resultArr = arr.filter(function(element){
+    return element > 0 && element % 3 === 0;
+  }).map(function(num){
+    return num * 10;
+  });
 
   // Ваш код
 
